de9b54b9c0688cadba7390c8cab4cdf783673faa
alxmart <alxmartinez@gmail.com>
2024-01-28 10:24:05 -0300

    Added code to generate random numbers and show on activity.

--- app/src/main/java/com/luizafmartinez/m02_sorteio/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/m02_sorteio/MainActivity.kt
@@ -2,10 +2,24 @@ package com.luizafmartinez.m02_sorteio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun sortear(view: View) {
+
+        val textoResultado = findViewById<TextView>(R.id.tvResultado)
+        val numero = Random().nextInt(61)
+        textoResultado.setText("Número gerado: $numero")
+    }
+
 }
+
+
